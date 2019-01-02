6d702fcc6960a7af88fde98e635f84aa2df52a4d
Jukka Päivinen <jukka.paivinen1@gmail.com>
2019-01-02 13:00:03 +0200

    new overlay, responsve text size

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var mapAddress = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/25.02372,60.22122,13.2,0,0/500x500?access_token=pk.eyJ1IjoibWlra29hdGEiLCJhIjoiY2l5Y3V5czBjMDA4ZzJ3cXBibXZhaDkzciJ9.F-rRrrNLnB0MfKtcosgmxA";
-var routeName = "Viikin 10k";
+var mapAddress = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/static/25.01939,60.22021,13.1,0,0/800x800?access_token=pk.eyJ1IjoibWlra29hdGEiLCJhIjoiY2l5Y3V5czBjMDA4ZzJ3cXBibXZhaDkzciJ9.F-rRrrNLnB0MfKtcosgmxA";
+var routeName = "Marathon des Sables";
 var routeDate = "01/01/2019 15:30";
 imgmod.generateImage(mapAddress, routeName, routeDate);
 
--- imgmodule.js
+++ imgmodule.js
@@ -3,18 +3,23 @@ var fs = require('fs');
 var text2png = require('text2png');
 
 exports.generateImage = function (mapAddress, routeName, routeDate) {
-    fs.writeFileSync('./public/routeName.png', text2png(routeName, {
-        color: '#03abc7',
-        font: '32px OpenSans',
+    var nameLength = routeName.length;
+    var nameSize = 53;
+    if (nameLength > 18) {
+        nameSize = nameSize * ( 1 - (nameLength - 18)*0.05)
+    }
+
+    fs.writeFileSync('./public/routeName.png', text2png(routeName.toUpperCase(), {
+        color: '#ffffff',
+        font: nameSize + 'px OpenSans',
         localFontPath: 'OpenSans-ExtraBoldItalic.ttf',
         localFontName: 'OpenSans',
-        padding: 20
+        paddingRight: 5
     }));
 
     fs.writeFileSync('./public/routeDate.png', text2png(routeDate, {
-        color: '#383838',
-        font: '16px Open Sans',
-        padding: 20
+        color: '#ffffff',
+        font: '30px Open Sans'
     }));
 
     var p1 = Jimp.read(mapAddress);
@@ -23,7 +28,7 @@ exports.generateImage = function (mapAddress, routeName, routeDate) {
     var p4 = Jimp.read("./public/routeDate.png");
 
 Promise.all([p1, p2, p3, p4]).then(images => {
-    images[0].composite(images[1], 0, 0).composite(images[2], 85, 0).composite(images[3], 90, 37).write("./public/kartta.png");
+    images[0].composite(images[1], 0, 0).composite(images[2], 155, 725-nameSize).composite(images[3], 157, 725).write("./public/kartta.png");
 });
 
 }
